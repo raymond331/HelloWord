c0b4da0d58bd17919c3e2be2570e59542e590259
raymond li <raymond_li@manulife.com>
2024-03-21 13:48:05 +0800

    Initial Commit

---
+++ HelloWord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWord
+//
+//  Created by Raymond Li （李英杰） on 2024/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWord/HelloWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWordApp.swift
+//  HelloWord
+//
+//  Created by Raymond Li （李英杰） on 2024/3/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
